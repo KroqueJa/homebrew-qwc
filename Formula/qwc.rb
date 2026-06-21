c418099b7cb9b39376bdd0f0fcf02962ca4d9dfb
Ji Krochmal <ji.krochmal@gmail.com>
2026-06-21 14:32:20 +0200

    Bump qwc to 0.3.0

--- Formula/qwc.rb
+++ Formula/qwc.rb
@@ -1,20 +1,20 @@
   class Qwc < Formula
     desc "A utility to count things. Quickly."
     homepage "https://github.com/KroqueJa/qwc"
-    version "0.2.1"
+    version "0.3.0"
     license "MIT"
 
     on_macos do
       on_arm do
         url "https://github.com/KroqueJa/qwc/releases/download/v#{version}/qwc-macos-arm64.tar.gz"
-        sha256 "8796b6e53ce038a9b0a125340c645fe56c282066318838933cb4a30dfe3b4f38"
+        sha256 "a0cb1479d409b97ae2d123a18668efd85053955e0c025e6f7e2d7dedb19bac26"
       end
     end
 
     on_linux do
       on_intel do
         url "https://github.com/KroqueJa/qwc/releases/download/v#{version}/qwc-linux-x86_64.tar.gz"
-        sha256 "7f2fb282c1b3d4caa118deddb92385485de6dda1d4323cc319ad70f42d4dc152"
+        sha256 "fc87a31b07a6cb40b32a49ccbf6b883f94de9962777ab5e52771de9b7bd51b75"
       end
     end
   
